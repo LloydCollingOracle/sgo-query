510edabefc02732e3e5e0f4e24c67da73cc995a4
rhyscharlton <rhyscharlton@edc181ce-0fe3-11de-aa17-9100d24c02a7>
2009-09-30 15:21:24 +0000

    Added multiple ResultSets support to The ResultSetFormatter.

--- src/main/java/org/sgodden/query/ResultSetFormatter.java
+++ src/main/java/org/sgodden/query/ResultSetFormatter.java
@@ -15,86 +15,91 @@ import org.jdom.output.XMLOutputter;
  */
 public class ResultSetFormatter {
 
-	private ResultSetCellRenderer renderer;
+    private ResultSetCellRenderer renderer;
 
-	public ResultSetFormatter() {
-		renderer = new DefaultResultSetCellRenderer();
-	}
+    public ResultSetFormatter() {
+        renderer = new DefaultResultSetCellRenderer();
+    }
 
-	/**
-	 * Sets the cell renderer to be used to render particular cell values.
-	 * 
-	 * @param renderer
-	 *            the renderer to use.
-	 */
-	public void setCellRenderer(ResultSetCellRenderer renderer) {
-		this.renderer = renderer;
-	}
+    /**
+     * Sets the cell renderer to be used to render particular cell values.
+     * 
+     * @param renderer
+     *            the renderer to use.
+     */
+    public void setCellRenderer(ResultSetCellRenderer renderer) {
+        this.renderer = renderer;
+    }
 
-	/**
-	 * Transforms the passed result set to XML.
-	 * 
-	 * @param rs
-	 *            the result set to transform.
-	 * @param out
-	 *            the output stream to which to write the XML.
-	 */
-	public void toXml(ResultSet rs, OutputStream out) {
-		toXml(rs, out, false);
-	}
+    /**
+     * Adds the results of a query to a root xml element.
+     * 
+     * @param rs
+     *            the result set to execute
+     * @param root
+     *            the root element to add new elements to
+     */
+    private void toXml(ResultSet rs, Element root) {
 
-	/**
-	 * Transforms the passed result set to XML.
-	 * 
-	 * @param rs
-	 *            the result set to transform.
-	 * @param out
-	 *            the output stream to which to write the XML.
-	 * @param pretty
-	 *            whether to prettify the returned XML.
-	 */
-	public void toXml(ResultSet rs, OutputStream out, boolean pretty) {
-		Element root = new Element("ResultSet");
-		Document doc = new Document(root);
-		Query q = rs.getQuery();
-		for (int row = 0; row < rs.getRowCount(); row++) {
-			ResultSetRow rsrow = rs.getRow(row);
-			Element rowE = new Element(getLastPart(q.getObjectClassName()));
-			root.addContent(rowE);
-			for (int col = 0; col < rsrow.getColumns().length; col++) {
-				ResultSetColumn column = rsrow.getColumns()[col];
-				QueryColumn qc = rs.getQuery().getColumns().get(col);
-				Element colE = new Element(legaliseColumnName(qc
-						.getAttributePath()));
-				Object value = column.getValue();
-				if (value != null) {
-					colE.setText(renderer.renderString(rs, value, col, row));
-				}
-				rowE.addContent(colE);
-			}
-		}
-		XMLOutputter xmlout = new XMLOutputter();
-		if (pretty) {
-			xmlout.setFormat(Format.getPrettyFormat());
-		}
-		try {
-			xmlout.output(doc, out);
-		} catch (Exception e) {
-			throw new RuntimeException("Error generating XML", e);
-		}
+        Query q = rs.getQuery();
+        for (int row = 0; row < rs.getRowCount(); row++) {
+            ResultSetRow rsrow = rs.getRow(row);
+            Element rowE = new Element(getLastPart(q.getObjectClassName()));
+            root.addContent(rowE);
+            for (int col = 0; col < rsrow.getColumns().length; col++) {
+                ResultSetColumn column = rsrow.getColumns()[col];
+                QueryColumn qc = rs.getQuery().getColumns().get(col);
+                Element colE = new Element(legaliseColumnName(qc
+                        .getAttributePath()));
+                Object value = column.getValue();
+                if (value != null) {
+                    colE.setText(renderer.renderString(rs, value, col, row));
+                }
+                rowE.addContent(colE);
+            }
+        }
+    }
 
-	}
+    /**
+     * Transforms the passed result sets to XML.
+     * 
+     * @param out
+     *            the output stream to which to write the XML.
+     * @param pretty
+     *            whether to prettify the returned XML.
+     * 
+     * @param resultSets
+     *            the result sets to transform.
+     */
+    public void toXml(OutputStream out, boolean pretty, ResultSet... resultSets) {
+        Element root = new Element("ResultSet");
+        Document doc = new Document(root);
 
-	private String legaliseColumnName(String columnName) {
-		return columnName.replaceAll(" ", "");
-	}
+        for (ResultSet resultSet : resultSets) {
+            toXml(resultSet, root);
+        }
 
-	private String getLastPart(String s) {
-		String ret = s;
-		if (ret.indexOf('.') != -1) {
-			ret = ret.substring(ret.lastIndexOf('.') + 1, ret.length());
-		}
-		return ret;
-	}
+        XMLOutputter xmlout = new XMLOutputter();
+        if (pretty) {
+            xmlout.setFormat(Format.getPrettyFormat());
+        }
+        try {
+            xmlout.output(doc, out);
+        } catch (Exception e) {
+            throw new RuntimeException("Error generating XML", e);
+        }
+    }
+
+    private String legaliseColumnName(String columnName) {
+        return columnName.replaceAll(" ", "");
+    }
+
+    private String getLastPart(String s) {
+        String ret = s;
+        if (ret.indexOf('.') != -1) {
+            ret = ret.substring(ret.lastIndexOf('.') + 1, ret.length());
+        }
+        return ret;
+    }
 
 }
--- src/test/java/org/sgodden/query/service/ResultSetFormatterTest.java
+++ src/test/java/org/sgodden/query/service/ResultSetFormatterTest.java
@@ -13,46 +13,47 @@ import org.sgodden.query.ResultSetFormatter;
 import org.sgodden.query.ResultSetRow;
 
 public class ResultSetFormatterTest {
-	
-	public static void main(String[] args) {
-		Query q = new Query();
-		q.setObjectClassName("org.example.Order");
-		q.addColumn("orderNumber");
-		q.addColumn("customerName");
-		q.addColumn("date");
-		
-		ResultSet rs = new ResultSet();
-		rs.setQuery(q);
-		
-		List<ResultSetRow> rows = new ArrayList<ResultSetRow>();
-		for (int i = 0; i < 2; i++) {
-			rows.add(makeRow(new Object[]{"number " + i, "name " + i, new LocalDate()}));
-		}
-		
-		rs.setCachedPageRows(rows);
-		rs.setRowCount(2);
-		
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		
-		new ResultSetFormatter().toXml(rs, out, true);
-		
-		System.out.println(out.toString());
-		
-	}
-	
-	private static ResultSetRow makeRow(Object[] values) {
-		ResultSetRow row = new ResultSetRow();
-		ResultSetColumn[] cols = new ResultSetColumn[values.length];
-		int i = 0;
-		for (Object value : values) {
-			ResultSetColumn col = new ResultSetColumn();
-			col.setDataType(DataType.STRING);
-			col.setValue(value);
-			cols[i] = col;
-			i++;
-		}
-		row.setColumns(cols);
-		return row;
-	}
+
+    public static void main(String[] args) {
+        Query q = new Query();
+        q.setObjectClassName("org.example.Order");
+        q.addColumn("orderNumber");
+        q.addColumn("customerName");
+        q.addColumn("date");
+
+        ResultSet rs = new ResultSet();
+        rs.setQuery(q);
+
+        List<ResultSetRow> rows = new ArrayList<ResultSetRow>();
+        for (int i = 0; i < 2; i++) {
+            rows.add(makeRow(new Object[] { "number " + i, "name " + i,
+                    new LocalDate() }));
+        }
+
+        rs.setCachedPageRows(rows);
+        rs.setRowCount(2);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        new ResultSetFormatter().toXml(out, true, rs);
+
+        System.out.println(out.toString());
+
+    }
+
+    private static ResultSetRow makeRow(Object[] values) {
+        ResultSetRow row = new ResultSetRow();
+        ResultSetColumn[] cols = new ResultSetColumn[values.length];
+        int i = 0;
+        for (Object value : values) {
+            ResultSetColumn col = new ResultSetColumn();
+            col.setDataType(DataType.STRING);
+            col.setValue(value);
+            cols[i] = col;
+            i++;
+        }
+        row.setColumns(cols);
+        return row;
+    }
 
 }
